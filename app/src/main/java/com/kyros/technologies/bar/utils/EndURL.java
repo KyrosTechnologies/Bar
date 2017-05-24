0858edc21f71b726ef32ddffff8c82a3f9f18e55
RohinGnanasekar <cserohin@gmail.com>
2017-05-24 18:19:11 +0530

    Latest Api Integration

--- app/src/main/java/com/kyros/technologies/bar/utils/EndURL.java
+++ app/src/main/java/com/kyros/technologies/bar/utils/EndURL.java
@@ -8,4 +8,5 @@ public class EndURL {
 
 //    public static final String URL="http://192.168.0.111:8080/Bar/rest/";
         public static final String URL="http://webapp-170524162357.azurewebsites.net/rest/";
+
 }
