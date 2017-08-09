ab5323d68165a714c85a8e04b368058c89715623
Arasu378 <v.t.a378@gmail.com>
2017-08-09 19:31:37 +0530

    url changed

--- app/src/main/java/com/kyros/technologies/bar/utils/EndURL.java
+++ app/src/main/java/com/kyros/technologies/bar/utils/EndURL.java
@@ -7,6 +7,11 @@ package com.kyros.technologies.bar.utils;
 public class EndURL {
 
      //public static final String URL="http://192.168.0.102:8080/Bar/rest/";
-        public static final String URL="http://kyrosbar.ap-south-1.elasticbeanstalk.com/rest/";
+
+    //PRODUCTION VERSION URL
+       // public static final String URL="http://kyrosbar.ap-south-1.elasticbeanstalk.com/rest/";
+
+    //development version url
+        public static final String URL="http://developmentkyrosbar.ap-south-1.elasticbeanstalk.com/rest/";
 
 }
